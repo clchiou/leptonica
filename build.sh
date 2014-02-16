1debfcfaeb59eb9b9af9647a52e6d788232bfd05
Che-Liang Chiou <clchiou@gmail.com>
2014-02-15 19:25:16 -0800

    Add ports build info

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright (c) 2014 Che-Liang Chiou. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+export EXTRA_CONFIGURE_ARGS="--disable-programs"
+
+ConfigureStep() {
+  Banner "Copy working directory"
+  rsync -av --delete ${START_DIR}/ ${NACL_PACKAGES_REPOSITORY}/${PACKAGE_DIR}
+  DefaultConfigureStep
+}
